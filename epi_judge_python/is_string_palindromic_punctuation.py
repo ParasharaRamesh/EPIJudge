a8257bed2f407b5bee56f9ac233ac505d5786d64
Parashara <parasharar@gmail.com>
2021-02-14 13:23:20 +0530

    string palindrome variation

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -3,11 +3,24 @@ from test_framework import generic_test
 
 def is_palindrome(s: str) -> bool:
     # TODO - you fill in here.
+    start = 0
+    end = len(s) -1
+    while start < end:
+        startCh = s[start]
+        endCh = s[end]
+        if startCh.isalnum() and endCh.isalnum() and startCh.lower() == endCh.lower():
+            start +=1
+            end -=1
+        elif not startCh.isalnum():
+            start +=1
+        elif not endCh.isalnum():
+            end -=1
+        else:
+            return False
+
     return True
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'is_string_palindromic_punctuation.py',
-            'is_string_palindromic_punctuation.tsv', is_palindrome))
+    # print(is_palindrome("a, man nama!."))
+    exit(generic_test.generic_test_main('is_string_palindromic_punctuation.py','is_string_palindromic_punctuation.tsv', is_palindrome))
