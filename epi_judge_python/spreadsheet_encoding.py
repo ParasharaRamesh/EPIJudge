eac0a442b4c3848cbd118c736c1bad064c585b24
Parashara <parasharar@gmail.com>
2021-02-14 12:54:04 +0530

    convert spreadsheet column encoding -> int

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,13 +1,20 @@
+import string
+
 from test_framework import generic_test
 
 
 def ss_decode_col_id(col: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    col = col.upper()
+    charToValueMap = {chr(ord("A") + i - 1): i for i in range(1, 27)}
+    res = 0
+    for i , ch in enumerate(reversed(col)):
+        res += (26 ** i) *  charToValueMap[ch]
+    return res
+
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('spreadsheet_encoding.py',
-                                       'spreadsheet_encoding.tsv',
-                                       ss_decode_col_id))
+    # print(ss_decode_col_id("D"))
+    # print(ss_decode_col_id("ZZ"))
+    # print(ss_decode_col_id("AA"))
+    exit(generic_test.generic_test_main('spreadsheet_encoding.py','spreadsheet_encoding.tsv',ss_decode_col_id))
