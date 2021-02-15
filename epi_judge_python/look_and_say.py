3050f7090fac500136dcfa1ae2cc03cd916631ed
Parashara <parasharar@gmail.com>
2021-02-15 10:40:19 +0530

    look and say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,12 +1,42 @@
 from test_framework import generic_test
 
+def say_next(prev_say):
+    next_say = ""
+    i = 0
+    while i < len(prev_say)-1:
+        #count forward until next diff element appears
+        curr = prev_say[i]
+        j = i+1
+        while j < len(prev_say) and prev_say[j] == curr:
+            j+=1
+        count = j - i
+        next_say += f"{count}{curr}"
+        i = j
+
+    #worry about last character being unique
+    if len(next_say) == 0 or prev_say[-1] != next_say[-1]:
+        next_say += f"1{prev_say[-1]}"
+
+    return next_say
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    i = 2
+    currSay = "1"
+    while i <= n:
+        currSay = say_next(currSay)
+        i+=1
+    return currSay
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('look_and_say.py', 'look_and_say.tsv',
-                                       look_and_say))
+    # print(say_next("1"))
+    # print(say_next("12"))
+    # print(say_next("11"))
+    # print(say_next("11122"))
+    # print(say_next("111223333"))
+    # print(say_next("111223333444"))
+    # print(look_and_say(1))
+    # print(look_and_say(2))
+    # print(look_and_say(3))
+    # print(look_and_say(8))
+    exit(generic_test.generic_test_main('look_and_say.py', 'look_and_say.tsv',look_and_say))
