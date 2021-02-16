f89ea6830cd6654a5969540b309b481afb9dc166
Parashara <parasharar@gmail.com>
2021-02-17 00:35:10 +0530

    run length encoding and decoding done

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -3,13 +3,33 @@ from test_framework.test_failure import TestFailure
 
 
 def decoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
-
+    dec = ""
+    i = 0
+    while i < len(s):
+        num = s[i]
+        j = i+1
+        while j < len(s) and s[j].isnumeric():
+            num += s[j]
+            j+=1
+        char = s[j]
+        dec += char*int(num)
+        i = j+1
+    return dec
 
 def encoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    enc = ""
+    i = 0
+    while i < len(s):
+        curr = s[i]
+        count = 1
+        j = i+1
+        while j < len(s) and s[j] == curr:
+            count +=1
+            j+=1
+        enc += f"{count}{curr}"
+        i+=count
+
+    return enc
 
 
 def rle_tester(encoded, decoded):
@@ -20,7 +40,10 @@ def rle_tester(encoded, decoded):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('run_length_compression.py',
-                                       'run_length_compression.tsv',
-                                       rle_tester))
+    # s = "10q13a8b2m17c"
+    # d = decoding(s)
+    # e = encoding(d)
+    # print(d)
+    # print(e)
+    # print(s == e)
+    exit(generic_test.generic_test_main('run_length_compression.py','run_length_compression.tsv',rle_tester))
