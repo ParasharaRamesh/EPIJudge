fecf7429c600e31ebe44f967063bcdecbff8ac79
Parashara <parasharar@gmail.com>
2021-03-02 22:33:55 +0530

    do terminated linked lists overlap

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -6,9 +6,51 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+def getLength(l):
+    curr = l
+    l = 0
+    while curr:
+        curr = curr.next
+        l+=1
+    return l
+
+def getNode(l, d):
+    curr = l
+    i = 0
+    while curr and i<d:
+        curr = curr.next
+        i+=1
+    if i < d:
+        return None
+    return curr
+
+def walkTogether(currA,currB):
+    while currA != currB:
+        currA = currA.next
+        currB = currB.next
+    if currA == None:
+        return None
+    else:
+        return currA
+
 def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+    a = getLength(l0)
+    b = getLength(l1)
+
+    diff = a-b
+    if diff > 0:
+        #a is the longer list, go diff + 1 in a , go diff in b and compare
+        currA = getNode(l0, diff)
+        currB = l1
+    elif diff < 0:
+        #b is the longer list, go diff + 1 in b , go diff in a and compare
+        currA = l0
+        currB = getNode(l1, -1 * diff)
+    else:
+        #do one by one traversal until we see a common node
+        currA = l0
+        currB = l1
+    return walkTogether(currA, currB)
 
 
 @enable_executor_hook
@@ -38,7 +80,4 @@ def overlapping_no_cycle_lists_wrapper(executor, l0, l1, common):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('do_terminated_lists_overlap.py',
-                                       'do_terminated_lists_overlap.tsv',
-                                       overlapping_no_cycle_lists_wrapper))
+    exit(generic_test.generic_test_main('do_terminated_lists_overlap.py','do_terminated_lists_overlap.tsv',overlapping_no_cycle_lists_wrapper))
