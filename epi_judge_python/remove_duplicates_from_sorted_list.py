a3129256f3bba53fa6464184d7252c12283b2b5e
Parashara <parasharar@gmail.com>
2021-03-07 20:09:33 +0530

    remove duplicates from sorted linked list

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -3,14 +3,33 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+def get_length(L):
+    curr = L
+    length = 0
+    while curr:
+        curr = curr.next
+        length +=1
+    return length
 
 def remove_duplicates(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    curr = L
+    firstOcc = curr
+    while curr:
+        if curr.data != firstOcc.data:
+            firstOcc.next = curr
+            firstOcc = curr
+        curr = curr.next
+
+    if get_length(firstOcc) > 1:
+        firstOcc.next = None
+
+    return L
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'remove_duplicates_from_sorted_list.py',
-            'remove_duplicates_from_sorted_list.tsv', remove_duplicates))
+    # d = ListNode(1,None)
+    # c = ListNode(1,d)
+    # b = ListNode(1,c)
+    # a = ListNode(1,b)
+    # remove_duplicates(a)
+    exit(generic_test.generic_test_main('remove_duplicates_from_sorted_list.py','remove_duplicates_from_sorted_list.tsv', remove_duplicates))
