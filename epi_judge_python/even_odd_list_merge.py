68dcd47666598eed0082ed3ce4163da1629015b8
Parashara <parasharar@gmail.com>
2021-03-08 11:54:16 +0530

    even odd linked list merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,12 +5,38 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L == None or L.next == None or L.next.next == None:
+        return L
+
+    evenHead = evenTail = L
+    oddHead = oddTail = L.next
+    curr = oddTail.next
+
+    evenTail.next = None
+    oddTail.next = None
+
+    isEven = True
+    while curr:
+        after = curr.next
+        if isEven:
+            evenTail.next = curr
+            evenTail = curr
+        else:
+            oddTail.next = curr
+            oddTail = curr
+        curr.next = None
+        isEven ^= True
+        curr = after
+
+    evenTail.next = oddHead
+    return evenHead
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('even_odd_list_merge.py',
-                                       'even_odd_list_merge.tsv',
-                                       even_odd_merge))
+    # e = ListNode(5,None)
+    # d = ListNode(4,e)
+    # c = ListNode(3,d)
+    # b = ListNode(2,c)
+    # a = ListNode(1,b)
+    # print(even_odd_merge(a))
+    exit(generic_test.generic_test_main('even_odd_list_merge.py','even_odd_list_merge.tsv',even_odd_merge))
