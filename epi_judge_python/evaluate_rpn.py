bc1feb0b2eba7af00e581816f750916d49172014
Parashara <parasharar@gmail.com>
2021-03-08 17:50:51 +0530

    evaluate rpn

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,12 +1,41 @@
 from test_framework import generic_test
 
 
+def compute(a, b, op):
+    if op == "+":
+        return str(int(a) + int(b))
+    elif op == "-":
+        return str(int(a) - int(b))
+    elif op == "*":
+        return str(int(a) * int(b))
+    else:
+        return str(int(a) // int(b))
+
+
+def isNumeric(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    ops = expression.split(",")
+    opsStack = []
+    for op in ops:
+        if isNumeric(op):
+            opsStack.append(op)
+        else:
+            # take the top two and evaluate and put it back in
+            b = opsStack.pop(-1)
+            a = opsStack.pop(-1)
+            val = compute(a, b, op)
+            opsStack.append(val)
+
+    return int(opsStack.pop())
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('evaluate_rpn.py', 'evaluate_rpn.tsv',
-                                       evaluate))
+    # print(evaluate("-641,6,/,28,/"))
+    exit(generic_test.generic_test_main('evaluate_rpn.py', 'evaluate_rpn.tsv',evaluate))
