0194465f0fd7bef999b9e6c4a0bc24dd50080202
Parashara <parasharar@gmail.com>
2021-03-14 13:55:55 +0530

    is binary tree height balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,14 +1,22 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from math import fabs
+
+
+def getHeight(node: BinaryTreeNode) -> int:
+    if node == None:
+        return 0
+    return 1 + max(getHeight(node.left), getHeight(node.right))
 
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+    if tree:
+        leftSubTreeHeight = getHeight(tree.left)
+        rightSubTreeHeight = getHeight(tree.right)
+        diff = fabs(leftSubTreeHeight - rightSubTreeHeight)
+        isThisNodeBalanced = diff <= 1
+        return isThisNodeBalanced and is_balanced_binary_tree(tree.left) and is_balanced_binary_tree(tree.right)
     return True
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('is_tree_balanced.py',
-                                       'is_tree_balanced.tsv',
-                                       is_balanced_binary_tree))
+    exit(generic_test.generic_test_main('is_tree_balanced.py','is_tree_balanced.tsv',is_balanced_binary_tree))
