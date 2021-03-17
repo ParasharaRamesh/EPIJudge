69d52684206a69406f8263623fcb7fd809d4f5eb
Parashara <parasharar@gmail.com>
2021-03-17 18:26:28 +0530

    inorder recursive

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -3,13 +3,21 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def inorder_recursive(tree,inorderList):
+    if tree:
+        inorder_recursive(tree.left, inorderList)
+        inorderList.append(tree.data)
+        inorder_recursive(tree.right, inorderList)
+
+def inorder_traversal_recursive(tree: BinaryTreeNode) -> List[int]:
+    inorderList = []
+    inorder_recursive(tree, inorderList)
+    return inorderList
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
-
+    inorderList = []
+    #TODO
+    return inorderList
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',
-                                       inorder_traversal))
+    exit(generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',inorder_traversal))
