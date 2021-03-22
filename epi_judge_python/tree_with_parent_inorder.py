cbd786bf1a7ade132fff49316e1ec0869e1ff7eb
Parashara <parasharar@gmail.com>
2021-03-22 18:06:26 +0530

    inorder traversal in constant space

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -2,11 +2,23 @@ from typing import List
 
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
-
+from successor_in_tree import *
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    inorder = []
+    leftMost = leftMostNode(tree)
+
+    curr = leftMost
+    inorder.append(curr.data)
+    while True:
+        successor = find_successor(curr)
+        if successor:
+            inorder.append(successor.data)
+            curr = successor
+        else:
+            break
+
+    return inorder
 
 
 if __name__ == '__main__':
