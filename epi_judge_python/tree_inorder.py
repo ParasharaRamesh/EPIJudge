5aaf0e367ca727f96a2eef64597f1bfb6766bbd9
Parashara <parasharar@gmail.com>
2021-03-22 16:57:38 +0530

    inorder iterative

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -3,21 +3,44 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
-def inorder_recursive(tree,inorderList):
+def inorder_recursive(tree, inorderList):
     if tree:
         inorder_recursive(tree.left, inorderList)
         inorderList.append(tree.data)
         inorder_recursive(tree.right, inorderList)
+
 
 def inorder_traversal_recursive(tree: BinaryTreeNode) -> List[int]:
     inorderList = []
     inorder_recursive(tree, inorderList)
     return inorderList
 
+
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
     inorderList = []
-    #TODO
+    stackFrames = []
+    stackFrames.append([tree, 0])
+    while stackFrames:
+        currNode, currState = stackFrames[-1]
+
+        if currNode == None or currState == 3:
+            stackFrames.pop()
+            continue
+        elif currState == 0:
+            stackFrames[-1][1] += 1
+            stackFrames.append([currNode.left, 0])
+        elif currState == 1:
+            stackFrames[-1][1] += 1
+            inorderList.append(currNode.data)
+        elif currState == 2:
+            stackFrames[-1][1] += 1
+            stackFrames.append([currNode.right, 0])
+
     return inorderList
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',inorder_traversal))
+    # c = BinaryTreeNode(3,None, None)
+    # b = BinaryTreeNode(2,None, None)
+    # a = BinaryTreeNode(1,b, c)
+    # print(inorder_traversal(a))
+    exit(generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv', inorder_traversal))
