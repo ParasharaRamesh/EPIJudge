6fec61d8f2b6df0a4084e14065f1a6d9eec16f15
Parashara <parasharar@gmail.com>
2021-03-23 12:24:56 +0530

    computed the exterior of a binary tree

--- epi_judge_python/binary_tree_node.py
+++ epi_judge_python/binary_tree_node.py
@@ -1,3 +1,5 @@
+import math
+
 from test_framework.binary_tree_utils import (binary_tree_to_string,
                                               equal_binary_trees)
 
@@ -16,3 +18,23 @@ class BinaryTreeNode:
 
     def __str__(self):
         return self.__repr__()
+
+    @staticmethod
+    def constructBinaryTreeFromList(l):
+        n = len(l)
+        limit = int(math.ceil((n - 3) / 2))
+        binaryNodes = []
+        for x in l:
+            if x is not None:
+                binaryNodes.append(BinaryTreeNode(x))
+            else:
+                binaryNodes.append(None)
+        for i in range(limit+1):
+            l = 2 * i + 1
+            r = 2 * i + 2
+            if binaryNodes[i]:
+                if l < n:
+                    binaryNodes[i].left = binaryNodes[l]
+                if r < n:
+                    binaryNodes[i].right = binaryNodes[r]
+        return binaryNodes[0]
--- epi_judge_python/tree_exterior.py
+++ epi_judge_python/tree_exterior.py
@@ -5,11 +5,51 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
+from tree_connect_leaves import *
 
+def isNodeALeaf(node):
+    if node:
+        return node.left == None and node.right == None
+    return False
+
+def computeLeftExterior(tree):
+    leftExterior = []
+    curr = tree
+    while curr and not isNodeALeaf(curr):
+        leftExterior.append(curr)
+        if curr.left:
+            curr = curr.left
+        elif curr.right:
+            curr = curr.right
+        else:
+            break
+    return leftExterior
+
+def computeRightExterior(tree):
+    rightExterior = []
+    curr = tree
+    while curr and not isNodeALeaf(curr):
+        rightExterior.append(curr)
+        if curr.right:
+            curr = curr.right
+        elif curr.left:
+            curr = curr.left
+        else:
+            break
+    return rightExterior
+
+def computeExterior(tree):
+    if tree:
+        leftExteriorExcludingLeavesAndParent = computeLeftExterior(tree.left)
+        rightExteriorExcludingLeavesAndParent = computeRightExterior(tree.right)
+        leaves = create_list_of_leaves(tree)
+        exteriorWithoutRoot = leftExteriorExcludingLeavesAndParent + leaves + list(reversed(rightExteriorExcludingLeavesAndParent))
+        return exteriorWithoutRoot if isNodeALeaf(tree) else [tree] + exteriorWithoutRoot
+    return []
 
 def exterior_binary_tree(tree: BinaryTreeNode) -> List[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return []
+    exterior = computeExterior(tree)
+    return exterior
 
 
 def create_output_list(L):
@@ -21,11 +61,9 @@ def create_output_list(L):
 @enable_executor_hook
 def create_output_list_wrapper(executor, tree):
     result = executor.run(functools.partial(exterior_binary_tree, tree))
-
     return create_output_list(result)
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('tree_exterior.py', 'tree_exterior.tsv',
-                                       create_output_list_wrapper))
+    # tree = BinaryTreeNode.constructBinaryTreeFromList([0, -6, 13, 2, -12, None, 3, -6])
+    # print(exterior_binary_tree(tree))
+    exit(generic_test.generic_test_main('tree_exterior.py', 'tree_exterior.tsv',create_output_list_wrapper))
