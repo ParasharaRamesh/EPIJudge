3ec6f16a77e42c6056aa95326027abae10b87182
Parashara <parasharar@gmail.com>
2021-03-24 17:22:03 +0530

    sort almost sorted array using heaps

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,22 @@
 from typing import Iterator, List
-
+from itertools import *
 from test_framework import generic_test
+from heapq import *
 
+def sort_approximately_sorted_array(sequence: Iterator[int],k: int) -> List[int]:
+    result = []
+    heap = list(islice(sequence, k))
+    heapify(heap)
+    while True:
+        try:
+            result.append(heappop(heap))
+            heappush(heap, next(sequence))
+        except StopIteration:
+            while heap:
+                result.append(heappop(heap))
+            break
 
-def sort_approximately_sorted_array(sequence: Iterator[int],
-                                    k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
@@ -14,7 +24,4 @@ def sort_approximately_sorted_array_wrapper(sequence, k):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'sort_almost_sorted_array.py', 'sort_almost_sorted_array.tsv',
-            sort_approximately_sorted_array_wrapper))
+    exit(generic_test.generic_test_main('sort_almost_sorted_array.py', 'sort_almost_sorted_array.tsv',sort_approximately_sorted_array_wrapper))
