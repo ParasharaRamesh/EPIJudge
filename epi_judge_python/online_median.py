8800f9c5960f11ee3c274c8e6aa56ab785eabf93
Parashara <parasharar@gmail.com>
2021-03-24 20:30:12 +0530

    compute median using a min and max heap

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,11 +1,41 @@
 from typing import Iterator, List
-
+from heapq import *
 from test_framework import generic_test
-
+from itertools import *
 
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    maxHeap = []
+    minHeap = []
+    medians = []
+    isEvenIndex = True
+    for x in sequence:
+        if isEvenIndex:
+            if len(maxHeap) == 0:
+                heappush(maxHeap, -x)
+            elif x <= minHeap[0]:
+                heappush(maxHeap, -x)
+            else:
+                mp = heappop(minHeap)
+                heappush(maxHeap, -mp)
+                heappush(minHeap, x)
+            medians.append(float(-maxHeap[0]))
+        else:
+            if len(minHeap) == 0:
+                if x >= -maxHeap[0]:
+                    heappush(minHeap, x)
+                else:
+                    mp = heappop(maxHeap) * -1
+                    heappush(minHeap, mp)
+                    heappush(maxHeap, -x)
+            elif x >= -maxHeap[0]:
+                heappush(minHeap, x)
+            else:
+                mp = heappop(maxHeap) * -1
+                heappush(minHeap, mp)
+                heappush(maxHeap, -x)
+            medians.append((minHeap[0] - maxHeap[0])/2)
+        isEvenIndex ^= True
+    return medians
 
 
 def online_median_wrapper(sequence):
@@ -13,6 +43,7 @@ def online_median_wrapper(sequence):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('online_median.py', 'online_median.tsv',
-                                       online_median_wrapper))
+    # print(online_median_wrapper([1, 0, 3, 5, 2, 0, 1]))
+    # print(online_median_wrapper([1, 2,3,4,5,6]))
+    # print(online_median_wrapper([4,3,2,1,0]))
+    exit(generic_test.generic_test_main('online_median.py', 'online_median.tsv', online_median_wrapper))
