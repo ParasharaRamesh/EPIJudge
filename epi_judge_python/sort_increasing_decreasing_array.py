1a3126e52238575ecd066dd3b4790b9676a46c2e
Parashara <parasharar@gmail.com>
2021-03-24 15:33:50 +0530

    sort increasing decreasing array

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,15 +1,27 @@
 from typing import List
-
+from heapq import *
 from test_framework import generic_test
+from collections import deque
+
+def find_each_sequence(a):
+    increasing = True
+    sequences = [deque([a[0]])]
+    for i in range(1,len(a)):
+        if increasing and a[i-1] <= a[i]:
+            sequences[-1].append(a[i])
+        elif not increasing and a[i-1] >= a[i]:
+            sequences[-1].appendleft(a[i])
+        else:
+            increasing ^= True
+            sequences.append(deque([a[i]]))
+    return list(map(lambda x: list(x), sequences))
+
 
 
 def sort_k_increasing_decreasing_array(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    sequences = find_each_sequence(A)
+    return list(merge(*sequences))
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('sort_increasing_decreasing_array.py',
-                                       'sort_increasing_decreasing_array.tsv',
-                                       sort_k_increasing_decreasing_array))
+    exit(generic_test.generic_test_main('sort_increasing_decreasing_array.py','sort_increasing_decreasing_array.tsv',sort_k_increasing_decreasing_array))
