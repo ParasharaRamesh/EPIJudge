f879319a03ebf1c1acc380ef944b4e6a91451f6a
Parashara <parasharar@gmail.com>
2021-03-30 12:07:59 +0530

    search smallest in a cyclically shifted sorted array

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -4,12 +4,16 @@ from test_framework import generic_test
 
 
 def search_smallest(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    smallest = None
+    n = len(A) - 1
+    l, r = 0, n
+    while l < r:
+        m = (l + r) // 2
+        if A[m] < A[-1]:
+            r = m
+        else:
+            l = m + 1
+    return l
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('search_shifted_sorted_array.py',
-                                       'search_shifted_sorted_array.tsv',
-                                       search_smallest))
+    exit(generic_test.generic_test_main('search_shifted_sorted_array.py', 'search_shifted_sorted_array.tsv',search_smallest))
